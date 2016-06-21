37ca8c9c6c02b3e443a3c3bf7050667d4629d8ac
slimane <smedini@gmail.com>
2016-06-21 18:49:30 +0200

    conf par env

--- scripts/portable/crawler.wiki.service.sh
+++ scripts/portable/crawler.wiki.service.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 sudo cp crawler.wiki.service /etc/systemd/system
+sudo chmod 664 /etc/systemd/system/crawler.wiki.service
 sudo systemctl enable crawler.wiki.service
 systemctl status crawler.wiki.service

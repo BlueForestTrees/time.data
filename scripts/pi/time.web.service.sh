bc8a69e5fedf57b80fb35d8843804b5c3c8726e6
slim <smedini@gmail.com>
2017-11-14 15:57:00 +0100

    time.api

--- scripts/pi/time.web.service.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-sudo ln -s /var/lib/time.data/scripts/pi/service/time.api.service /etc/systemd/system/time.api.service
-sudo chown 664 /var/lib/time.data/scripts/pi/service/time.api.service
-sudo systemctl enable time.api.service
-systemctl status time.api.service
